fe040e77988a6bdfefb38424571565180ee8df6e
合田和樹 <goda@eyemovic.com>
2019-10-01 16:08:34 +0900

    ざっくりライフサイクルを理解

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,20 @@
 var data = {a: 1};
 var vm = new Vue({
-  el: '#app',
-  data: data,
-});
+    el: '#app',
+    data: data,
 
+    beforeCreate: function () {
+        console.log("before Createしました。");
+    },
 
-// $watch はインスタンスメソッドです
-vm.$watch('a', function (newValue, oldValue) {
-  // このコールバックは `vm.a` の値が変わる時に呼ばれます
-  console.log('変更前は' + oldValue + 'でした。');
-  console.log('変更後は' + newValue + 'でした。');
+    created: function () {
+        console.log("createdしました。");
+    },
+
+    beforeUpdate: function () {
+        console.log("beforeUpdateしました。");
+    },
+    updated: function () {
+        console.log("updatedしました。");
+    }
 });
