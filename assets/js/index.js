2eaf9e9f1f1447efa321ec8d55a26bae6dc4cca2
合田和樹 <goda@eyemovic.com>
2019-10-25 17:22:13 +0900

    v-modelについて

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,8 @@
 var vm = new Vue({
     el: '#app',
+
     data: {
-        message: "これがめっさーげ",
-        firstName: "goda",
-        lastName: "kazuki",
-        fullName: "goda kazuki"
+        myName: ""
     },
-    computed: {
-        reversedMessage() {
-            return this.message.split("").reverse().join("");
-        },
-        test() {
-            return Date.now();
-        },
-    },
-
-    methods: {
-        now() {
-            console.log("nowが実行されました。");
-            return Date.now();
-        },
-    },
-
-    watch: {
-        firstName(val) {
-            this.fullName = val + " " + this.lastName
-        },
-        lastName(val) {
-            this.fullName = this.firstName + " " + val
-        }
-    }
+    methods: {}
 });
