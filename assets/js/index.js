9219687f0525d8885746f1ed60508ce8f8b3abaa
合田和樹 <goda@eyemovic.com>
2019-10-30 16:41:20 +0900

    watchを使ってカウントダウンボタン作成

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,31 @@ var vm = new Vue({
     el: '#app',
 
     data: {
-        myArray:[1,2,3,4,5]
+        resetSec: 0,
+        timerObj: null
+    },
+    methods: {
+        startTimer: function () {
+            this.resetSec = 5;
+
+            this.timerObj = setInterval(() => {
+                this.resetSec--;
+            }, 1000)
+        }
+    },
+
+    watch: {
+        resetSec: function () {
+            console.log(vm.resetSec);
+
+            if (vm.resetSec <= 0) {
+                console.log(vm.resetSec);
+
+                alert("制限時間です");
+                clearInterval(this.timerObj);
+            }
+
+        }
     }
+
 });
