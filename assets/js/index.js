b26e7c527ed5899fbbf2f2e774111fcdcb8eacae
eyemovic-goda <goda@eyemovic.com>
2019-10-31 22:22:02 +0900

    しゃっふるする動きをつけた。transition

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ var vm = new Vue({
 
     data: {
         addItem: "",
-        items: []
+        items: ["one","two","three","for","five"]
 
     },
     methods: {
@@ -14,7 +14,25 @@ var vm = new Vue({
         removeLast: function () {
             this.items.splice(this.items.length - 1, 1)
 
-        }
+        },
+        sortData:function () {
+            this.items.sort(function (a,b) {
+                if(a<b)return-1;
+                if(a>b)return 1;
+                return 0;
+            })
+        },
+        shuffleData:function () {
+            var buffer=[];
+            var len=this.items.length;
+            for(var i=len;len>0;len--){
+                var r=Math.floor(Math.random()*len);
+
+                buffer.push(this.items[r]);
+                this.items.splice(r,1);
+            }
+            this.items=buffer;
+        },
     },
 
     watch: {},
