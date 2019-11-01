cb385b78417006c265e9742f3b21417ffb885d69
合田和樹 <goda@eyemovic.com>
2019-11-01 13:47:33 +0900

    jsonファイルを読み込めるようにした。

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,31 @@
-var MyComponent = {
-    template: '<p class="my-comp">私は、 {{ myName }}です。 </p>',
-
-    props: {
-        myName: String,
-    },
-
-    created: function () {
-        if (this.myName == null) {
-            this.myName = "名無しの権兵衛"
-        }
-    },
-
-    data: function () {
-        return {}
-    },
-
-    methods: {}
-
-};
-
 var vm = new Vue({
     el: '#app',
 
-    components: {
-        "my-component": MyComponent
-    },
+    components: {},
 
     data: {
-        items: ['桃太郎', '浦島太郎', '金太郎', '三年寝太郎', '龍 の子太郎', null]
+        loadData: ""
     },
     computed: {},
 
-    methods: {},
+    methods: {
+        onFileChange: function (e) {
+
+            console.log(e);
+
+            var file = e.target.files[0];
+            if (file) {
+                var reader = new FileReader();
+                var vm = this;
+
+                reader.onload = function (e) {
+                    vm.loadData = JSON.parse(e.target.result);
+                };
+                reader.readAsText(file)
+            }
+
+        },
+    },
 
     watch: {},
 
