81e9098b3cb2aaf6ec3e655357af2344486fba67
合田和樹 <goda@eyemovic.com>
2019-11-01 11:24:18 +0900

    component,propsを学習

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,45 +1,35 @@
+var MyComponent = {
+    template: '<p class="my-comp">私は、 {{ myName }}です。 </p>',
+
+    props: {
+        myName: String,
+    },
+
+    created: function () {
+        if (this.myName == null) {
+            this.myName = "名無しの権兵衛"
+        }
+    },
+
+    data: function () {
+        return {}
+    },
+
+    methods: {}
+
+};
+
 var vm = new Vue({
     el: '#app',
 
-    data: {
-        addText: "",
-        todos: [
-            {done: false, text: "パンを買う"},
-            {done: false, text: "コーヒーを買う"}
-        ]
-    },
-    computed: {
-        /**
-         * 何件処理したかを計算する。
-         * @returns {number}
-         */
-        remaining: function () {
-            return this.todos.filter(function (val) {
-                return val.done;
-            }).length
-
-        }
+    components: {
+        "my-component": MyComponent
     },
 
-    methods: {
-        addToDo: function () {
-            if (this.addText === "") {
-                return
-            }
+    data: {},
+    computed: {},
 
-            this.todos.push({done: false, text: this.addText});
-            this.addText = "";
-        },
-
-        /**
-         * doneがtrueなら削除する
-         */
-        cleanToDo: function () {
-            this.todos = this.todos.filter(function (val) {
-                return val.done === false;
-            });
-        }
-    },
+    methods: {},
 
     watch: {},
 
