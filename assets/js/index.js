e8804bf7b5b49c0f45b88937e560fec52be56cdd
合田和樹 <goda@eyemovic.com>
2019-11-01 14:31:31 +0900

    ！！読了！！

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,52 @@
+var MyComponent = {
+    props: ["object"],
+    template: `
+<div style="width:300px;backgroundColor:#ffddaa;">
+<p style="backgroundColor:#ffa95e;">{{object.title}}</p>
+<p>解説:{{object.body}}</p> </div>`
+};
+
 var vm = new Vue({
     el: '#app',
 
-    components: {},
+    components: {
+        "my-product": MyComponent
+    },
 
     data: {
-        loadData: ""
+        dataArray: []
     },
     computed: {},
 
     methods: {
-        onFileChange: function (e) {
+        sortData: function () {
+            this.dataArray.sort(function (a, b) {
+                return (a.title < b.title ? -1 : 1);
+            })
+        },
 
-            console.log(e);
+        shuffleData: function () {
+            var buffer = [];
+            var len = this.dataArray.length;
 
+            for (var i = len; len > 0; len--) {
+                var r = Math.floor(Math.random() * len);
+                buffer.push(this.dataArray[r]);
+                this.dataArray.splice(r, 1);
+            }
+            this.dataArray = buffer;
+        },
+        loadData: function (e) {
             var file = e.target.files[0];
             if (file) {
                 var reader = new FileReader();
                 var vm = this;
-
                 reader.onload = function (e) {
-                    vm.loadData = JSON.parse(e.target.result);
+                    vm.dataArray = JSON.parse(e.target.result);
                 };
                 reader.readAsText(file)
             }
-
-        },
+        }
     },
 
     watch: {},
